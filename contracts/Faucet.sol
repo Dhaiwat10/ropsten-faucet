1b6b481237bda68136d8628c36c60c568385dda9
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-04-20 14:26:30 +0530

    Add withdraw restrictions to contract

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -6,8 +6,10 @@ import "hardhat/console.sol";
 
 contract Faucet {
     receive() external payable {}
+    address public creator = 0x0ED6Cec17F860fb54E21D154b49DAEFd9Ca04106;
 
     function withdraw (address payable recepient) public {
+        require(msg.sender == creator);
         recepient.transfer(100000000000000000);
     }
 }
